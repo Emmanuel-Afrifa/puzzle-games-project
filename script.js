d35364eae103b9a12aec6410e3ec5544f0e443c9
Emmanuel-Afrifa <emmaquame9@gmail.com>
2024-12-24 00:21:41 +0000

    I updated the coded by cutting the pieces properly as it's done in an actual jigsaw game. I also implemented the end game screen and added a few functionalities to the game

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // Global variables 
 let CANVAS = null; // Initializing the Canvas element
 let CONTEXT = null; // Initializing the context
-let SCALER =  0.5; 
+let SCALER =  0.4; 
 let SIZE = {
     x: 0,
     y: 0,
@@ -16,7 +16,8 @@ let BOTTOM_PLANE = null;
 let LEFT_PLANE = null;
 let RIGHT_PLANE = null;
 const PIECE_OFFSET = 20;
-
+let START_TIME = null;
+let END_TIME = null;
 
 
 const img = new Image();
@@ -27,34 +28,57 @@ img.src = "./pic.jpg";
 window.addEventListener('load', onload);
 
 function onload(){
+
+    END_TIME = null;
+    START_TIME = null;
+
+    const gameDifficulty = document.getElementById('game-difficulty');
+    gameDifficulty.value = 'very-easy'
+    gamePlayDifficulty(gameDifficulty.value);
+
+
+    startGame();
+}
+
+function startGame(){
+    const canvasDisplay = document.getElementById('rootCanvas')
+    canvasDisplay.style.display = 'none';
+
+    // Hiding the start Page
+    const startDisplay = document.getElementById('main-container');
+    startDisplay.style.display = 'flex';
+
+    // Hiding the end page
+    const endPageDisplay = document.getElementById('end-game-page');
+    endPageDisplay.style.display = 'none';
+
     const gameDifficulty = document.getElementById('game-difficulty');
     // set game play level 
     gameDifficulty.addEventListener('click', (event) => {
         const selectedLevel = gameDifficulty[gameDifficulty.selectedIndex].value;
         gamePlayDifficulty(gameDifficulty.value);
-        console.log(SIZE.row, SIZE.col)
     })
 
     const startButton = document.getElementById('start-game-button');
-    startButton.addEventListener('click', startGame);
-
-    // startGame();
+    startButton.addEventListener('click', gamePlay);
 }
 
-
-
-function startGame(){
+function gamePlay(){
+    START_TIME = new Date().getTime()
     CANVAS = document.getElementById("rootCanvas");
     CONTEXT = CANVAS.getContext('2d');
 
-    const canvasDisplay = document.getElementById('rootCanvas')
     // Showing the Game itself
+    const canvasDisplay = document.getElementById('rootCanvas')
     canvasDisplay.style.display = 'block';
-    const startDisplay = document.getElementById('main-container');
+
     // Hiding the start Page
+    const startDisplay = document.getElementById('main-container');
     startDisplay.style.display = 'none';
 
-
+    // Hiding the end page
+    const endPageDisplay = document.getElementById('end-game-page');
+    endPageDisplay.style.display = 'none';
 
     pieceMovements();
     resizeHandler();
@@ -63,6 +87,7 @@ function startGame(){
     window.addEventListener('resize', resizeHandler);
     randomizePieces();
     updateCanvas();
+
 }
 
 function gamePlayDifficulty(level){
@@ -145,6 +170,54 @@ function initializePieces(row, col){
             PIECES.push(new Piece(i,j));
         }
     }
+
+    let count = 0;
+    for (let i = 0; i < SIZE.row; i++){
+        for (let j = 0; j < SIZE.col; j++){
+            const selectedPiece = PIECES[count];
+
+            // Making the bottom tabs randomly with the exception of the 
+            // last row
+            if (i == SIZE.row - 1){
+                selectedPiece.bottom = null;
+            }
+            else {
+                const innerOuterTab = (Math.random() - 0.5) < 0 ? -1 : 1;
+                selectedPiece.bottom = innerOuterTab*(Math.random()*0.4+0.3);
+            }
+
+            // Maing the right tabs randomly with the exception of the 
+            // rightmots pieces
+            if (j == SIZE.col - 1){
+                selectedPiece.right = null;
+            }
+            else {
+                const innerOuterTab = (Math.random() - 0.5) < 0 ? -1 : 1;
+                selectedPiece.right = innerOuterTab*(Math.random()*0.4+0.3);
+            }
+
+            // Setting the top pieces tabs to be the opposite of the bottom tabs
+            // of the piece directly above them
+            if (i == 0){
+                selectedPiece.top = null;
+            }
+            else {
+                selectedPiece.top = -PIECES[count - SIZE.col].bottom;
+            }
+
+            // Setting the left tab in a similar way
+            if (j == 0){
+                selectedPiece.left = null;
+            }
+            else {
+                selectedPiece.left = -PIECES[count - 1].right;
+            }
+
+
+            count++;
+        }
+    }
+
 }
 
 function randomizePieces(){
@@ -168,8 +241,10 @@ function randomizePieces(){
     const leftRightWidth = SIZE.height;
 
     // numPiecesTop = Math.floor((rightStartPos - leftStartPos + pieceWidth)/pieceWidth);
-    numPiecesTop = Math.floor(topBottomWidth/(pieceWidth + 5));
-    numPiecesLeft = Math.floor( leftRightWidth / (pieceHeight + 5) );
+    const tabOffset = 0.15*Math.min(PIECES[0].height, PIECES[0].width);
+
+    numPiecesTop = Math.floor( CANVAS.width*0.9 /(pieceWidth + tabOffset + 5));
+    numPiecesLeft = Math.floor( leftRightWidth / (pieceHeight + tabOffset + 5));
 
     // let offsetX = (topBottomWidth - numPiecesTop*pieceWidth)/(numPiecesTop-1);
     // let offsetY = (leftRightWidth - numPiecesLeft*pieceHeight) / (numPiecesLeft - 1);
@@ -177,16 +252,15 @@ function randomizePieces(){
     // Building the top array
     allPieces = [];
     // const topPieces = [[topStartX, topStartPos]];
-
     for (let i=0; i<numPiecesTop; i++){
-        const newCoor = topStartX + i*(pieceWidth + 5);
+        const newCoor = topStartX + i*(pieceWidth + tabOffset + 5);
         allPieces.push([newCoor, topStartPos])
     }
 
     // Building the bottom array
     for (let i=0; i<numPiecesTop; i++){
         if (allPieces.length < SIZE.row * SIZE.col){
-            const newCoor = topStartX + 12 + i*(pieceWidth + 5);
+            const newCoor = topStartX + i*(pieceWidth + tabOffset + 5);
             allPieces.push([newCoor, bottomStartPos]);
         }
     }
@@ -194,7 +268,7 @@ function randomizePieces(){
     // Building the left array
     for (let i = 0; i<numPiecesLeft; i++){
         if (allPieces.length < SIZE.row * SIZE.col){
-            const yCoord = topStartY + i*(pieceHeight + 5);
+            const yCoord = topStartY + i*(pieceHeight + tabOffset + 5);
             allPieces.push([leftStartPos, yCoord]);
         }
     }
@@ -202,7 +276,7 @@ function randomizePieces(){
     // Building the Right array
     for (let i = 0; i<numPiecesLeft; i++){
         if (allPieces.length < SIZE.row * SIZE.col){
-            const yCoord = topStartY + i*(pieceHeight + 5);
+            const yCoord = topStartY + i*(pieceHeight + tabOffset + 5);
             allPieces.push([rightStartPos, yCoord]);
         }
     }
@@ -218,8 +292,9 @@ function randomizePieces(){
                     secondLayerIndex,
                     topStartY,
                     pieceHeight,
-                    leftStartPos - 2*8 - 2*pieceWidth,
-                    rightStartPos + 2*8 + 2*pieceWidth,
+                    leftStartPos - 2*(tabOffset + 5) - 2*pieceWidth,
+                    rightStartPos + 2*(tabOffset + 5) + 2*pieceWidth,
+                    (tabOffset + 5)
                 )
                 secondLayerIndex += 1;
             }
@@ -228,8 +303,9 @@ function randomizePieces(){
                     leftOverPiecesIndex,
                     topStartY,
                     pieceHeight,
-                    leftStartPos - 8 - pieceWidth,
-                    rightStartPos + 8 + pieceWidth,
+                    leftStartPos - (tabOffset + 5) - pieceWidth,
+                    rightStartPos + (tabOffset + 5) + pieceWidth,
+                    (tabOffset + 5)
                 )
             }
             allPieces.push(coords);
@@ -259,10 +335,10 @@ function randomizePieces(){
     }
 }
 
-function xOffset(leftOverPiecesIndex, topStartY, pieceHeight, leftOffset, rightOffset){
+function xOffset(leftOverPiecesIndex, topStartY, pieceHeight, leftOffset, rightOffset, verticalOffset){
     const isLeft = leftOverPiecesIndex % 2 == 0;
     const xCoord = isLeft ? leftOffset : rightOffset;
-    const yCoord = topStartY + Math.floor(leftOverPiecesIndex / 2)*(pieceHeight + 5);
+    const yCoord = topStartY + Math.floor(leftOverPiecesIndex / 2)*(pieceHeight + verticalOffset);
     return [xCoord, yCoord]
 
 }
@@ -303,8 +379,9 @@ function mouseUpEvent(){
     if (PIECE_SELECTED != null){
         if (PIECE_SELECTED.isClose()){
             PIECE_SELECTED.snapInPlace();
-            if (gameComplete(PIECE_SELECTED)){
-                console.log('Well Done')
+            if (gameComplete(PIECE_SELECTED) && END_TIME == null){
+                END_TIME = new Date().getTime();
+                gameOver();
             }
         }
     }
@@ -340,6 +417,63 @@ function getPressedPiece(location){
     return null;
 }
 
+// Defining the game over function
+function gameOver(){
+    const canvasDisplay = document.getElementById('rootCanvas')
+    // Hiding the Game itself
+    canvasDisplay.style.display = 'none';
+
+    // removing all the event listeners from the canvas element
+    removeCanvasEventListeners();
+    // resetting the game level to very-easy
+    const gameDifficulty = document.getElementById('game-difficulty');
+    gameDifficulty.value = 'very-easy';
+    gamePlayDifficulty(gameDifficulty.value);
+
+    const startDisplay = document.getElementById('main-container');
+    // Hiding the start Page
+    startDisplay.style.display = 'none';
+
+    // displaying the time taken
+    getTimeElapsed(END_TIME - START_TIME);
+
+    const endPageDisplay = document.getElementById('end-game-page');
+    // Showing the end page
+    endPageDisplay.style.display = 'flex';
+
+    const nextGameButton = document.getElementById('next-game-button');
+    nextGameButton.addEventListener('click', onload);
+}
+
+function getTimeElapsed(timeInMilleSeconds){
+    let seconds = Math.floor(timeInMilleSeconds/1000);
+    let actSecs = Math.floor((seconds%60));
+    let actMins = Math.floor((seconds % (60 * 60))/60);
+    let actHrs = Math.floor((seconds % (60*60*60))/(60*60));
+
+    let elapsedTime = 
+    actHrs.toString().padStart(2, '0');
+    elapsedTime += ':';
+    elapsedTime += actMins.toString().padStart(2, '0');
+    elapsedTime += ':';
+    elapsedTime += actSecs.toString().padStart(2, '0');
+
+    const timer = document.getElementById('time');
+    timer.textContent = elapsedTime;
+}
+
+// function to remove all the event listeners from the canvas page
+function removeCanvasEventListeners(){
+    function pieceMovements(){
+        CANVAS.removeEventListener('mousedown', mouseDownEvent);
+        CANVAS.removeEventListener('mousemove', mouseMoveEvent);
+        CANVAS.removeEventListener('mouseup', mouseUpEvent);
+        CANVAS.removeEventListener('touchstart', touchStartEvent);
+        CANVAS.removeEventListener('touchmove', touchMoveEvent);
+        CANVAS.removeEventListener('touchend', touchEndEvent);
+    }
+}
+
 // // Defining the pieces class
 class Piece{
     constructor(rowIndex, colIndex){
@@ -358,19 +492,179 @@ class Piece{
     // Function to draw/cut pieces
     draw(context){
         context.beginPath();
+        context.lineWidth = 0.01;
+        // context.strokeStyle = 'red';
+
+        const minLen = Math.min(this.width, this.height);
+        const neck = 0.03*minLen;
+        const tabWidth = 0.25*minLen;
+        const tabHeight = 0.15*minLen;
+
+        // context.rect(this.x, this.y, this.width, this.height);
+        // from top left
+        context.moveTo(this.x, this.y);
+        
+
+        // to top right
+        if (this.top){
+            context.lineTo(
+                this.x + this.width*Math.abs(this.top) - neck, this.y);
+            
+            context.bezierCurveTo(
+                this.x + this.width*Math.abs(this.top) - neck,
+                this.y - tabHeight * Math.sign(this.top)*0.2,
+
+                this.x + this.width *Math.abs(this.top) - tabWidth,
+                this.y - tabHeight * Math.sign(this.top),
+
+                this.x + this.width * Math.abs(this.top),
+                this.y - tabHeight*Math.sign(this.top)
+            );
+
+            context.bezierCurveTo(
+                this.x + this.width *Math.abs(this.top) + tabWidth,
+                this.y - tabHeight * Math.sign(this.top),
+
+                this.x + this.width*Math.abs(this.top) + neck,
+                this.y - tabHeight * Math.sign(this.top)*0.2,
+
+                this.x + this.width*Math.abs(this.top) + neck, 
+                this.y
+            )
+            
+            // context.lineTo(
+            //     this.x + this.width * Math.abs(this.top),
+            //     this.y - tabHeight*Math.sign(this.top)
+            // );
+            // context.lineTo(
+            //     this.x + this.width*Math.abs(this.top) + neck, this.y);
+        }
+        context.lineTo(this.x + this.width, this.y);
+        
+        // to bottom right
+        if (this.right){
+            context.lineTo(
+                this.x+this.width, this.y+this.height*Math.abs(this.right) - neck);
+
+            context.bezierCurveTo(
+                this.x + this.width - tabHeight*Math.sign(this.right)*0.2,
+                this.y + this.height * Math.abs(this.right) - neck,
+
+                this.x + this.width - tabHeight * Math.sign(this.right),
+                this.y + this.height*Math.abs(this.right) - tabWidth,
+
+                this.x + this.width - tabHeight * Math.sign(this.right),
+                this.y + this.height * Math.abs(this.right)
+            );
+
+            context.bezierCurveTo(
+                this.x + this.width - tabHeight*Math.sign(this.right),
+                this.y + this.height * Math.abs(this.right) + tabWidth,
+
+                this.x + this.width - tabHeight * Math.sign(this.right)*0.2,
+                this.y + this.height * Math.abs(this.right) + neck,
+
+                this.x + this.width,
+                this.y + this.height*Math.abs(this.right) + neck
+            );
+
+            // context.lineTo(
+            //     this.x+this.width - tabHeight*Math.sign(this.right),
+            //     this.y + this.height*Math.abs(this.right)
+            // );
+            context.lineTo(
+                this.x+this.width, this.y+this.height*Math.abs(this.right) + neck);
+        }
+        context.lineTo(this.x + this.width, this.y + this.height);
+
+        // to bottom left
+        if (this.bottom){
+            context.lineTo(
+                this.x + this.width*Math.abs(this.bottom) + neck, this.y + this.height);
+
+            context.bezierCurveTo(
+                this.x + this.width*Math.abs(this.bottom) + neck,
+                this.y + this.height + tabHeight * Math.sign(this.bottom)*0.2,
+
+                this.x + this.width*Math.abs(this.bottom) + tabWidth,
+                this.y + this.height + tabHeight*Math.sign(this.bottom),
+
+                this.x + this.width*Math.abs(this.bottom),
+                this.y + this.height + tabHeight*Math.sign(this.bottom)
+            );
+
+            context.bezierCurveTo(
+                this.x + this.width * Math.abs(this.bottom) - tabWidth,
+                this.y + this.height + tabHeight*Math.sign(this.bottom),
+
+                this.x + this.width * Math.abs(this.bottom) - neck,
+                this.y + this.height + tabHeight*Math.sign(this.bottom)*0.2,
+
+                this.x + this.width*Math.abs(this.bottom) - neck,
+                this.y + this.height
+            )
+
+            // context.lineTo(
+            //     this.x + this.width*Math.abs(this.bottom),
+            //     this.y + this.height + tabHeight*Math.sign(this.bottom)
+            // );
+            // context.lineTo(
+            //     this.x + this.width*Math.abs(this.bottom) - neck, this.y + this.height);
+        }
+        context.lineTo(this.x, this.y + this.height);
+
+        // to top left
+        if (this.left){
+            context.lineTo(this.x, this.y+this.height*Math.abs(this.left) + neck);
+
+            context.bezierCurveTo(
+                this.x + tabHeight * Math.sign(this.left)*0.2,
+                this.y + this.height * Math.abs(this.left) + neck,
+
+                this.x + tabHeight*Math.sign(this.left),
+                this.y + this.height * Math.abs (this.left) + tabWidth,
+
+                this.x + tabHeight*Math.sign(this.left),
+                this.y + this.height * Math.abs(this.left)
+            );
+
+            context.bezierCurveTo(
+                this.x + tabHeight * Math.sign(this.left),
+                this.y + this.height * Math.abs(this.left) - tabWidth,
+
+                this.x + tabHeight*Math.sign(this.left)*0.2,
+                this.y + this.height*Math.abs(this.left) - neck,
+
+                this.x,
+                this.y + this.height*Math.abs(this.left) - neck
+            )
+
+            // context.lineTo(
+            //     this.x + tabHeight*Math.sign(this.left),
+            //     this.y + this.height*Math.abs(this.left)
+            // );
+            // context.lineTo(this.x, this.y+this.height*Math.abs(this.left) - neck);
+        }
+        context.lineTo(this.x, this.y);
+
+        context.save();
+        context.clip();
+
+        const scaledTabHeight = Math.min(img.width/SIZE.col, img.height/SIZE.row)*tabHeight/minLen;
 
         context.drawImage(img,
-            this.colIndex*img.width/SIZE.col,
-            this.rowIndex*img.height/SIZE.row,
-            img.width/SIZE.col,
-            img.height/SIZE.row,
-            this.x,
-            this.y,
-            this.width,
-            this.height
+            this.colIndex*img.width/SIZE.col - scaledTabHeight,
+            this.rowIndex*img.height/SIZE.row - scaledTabHeight,
+            img.width/SIZE.col + scaledTabHeight*2,
+            img.height/SIZE.row + scaledTabHeight*2,
+            this.x - tabHeight,
+            this.y - tabHeight,
+            this.width + 2*tabHeight,
+            this.height + 2*tabHeight
         );
 
-        context.rect(this.x, this.y, this.width, this.height);
+        context.restore();
+
         context.stroke();
     }
 
